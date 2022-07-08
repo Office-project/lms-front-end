e95a05d379ddf20751c017dd7113c0c507a6f172
omoyele1669 <99410598+omoyele1669@users.noreply.github.com>
2022-07-08 09:01:29 +0100

    correcting the employee.js file

--- src/componets/AdminPages/NewEmployee/Employee/Employee.js
+++ src/componets/AdminPages/NewEmployee/Employee/Employee.js
@@ -4,7 +4,7 @@ import OptionService from "../../../Service/OptionService";
 import AdminServices from "../../../Service/AdminServices";
 
 const Employee = () => {
-    const [apply, setApply] = useState({});
+    const [apply, setApply] = useState({ gender: "MALE", role: "STAFF" });
     const [dept, setDept] = useState([]);
     const [locale, setLocale] = useState([]);
     const [message, setMessage] = useState("");
@@ -24,7 +24,7 @@ const Employee = () => {
     }, []);
 
     useEffect(() => {
-        OptionService.getLocationOption.then((resp) => {
+        OptionService.getLocationOption().then((resp) => {
             const newdata = {
                 "id": 0,
                 "name": "--nil--"
@@ -53,6 +53,8 @@ const Employee = () => {
         apply.departmentID = parseInt(num);
         apply.locationId = parseInt(num2);
 
+        console.log(apply);
+
 
         AdminServices.createStaff(apply).then((response) => {
             if (response.status === 201) {
@@ -61,168 +63,172 @@ const Employee = () => {
                 setMessage("unsuccessful")
             }
 
+        }).catch((error) => {
+            console.log(error);
+            setMessage("Not successful")
         })
 
     }
 
     return (
         <div>
-        <p>{message}</p>
-        {!isClosed &&
-            <form className={style.main} onSubmit={handleSubmit}>
-                <div >
+            <p>{message}</p>
+            {!isClosed &&
+                <form className={style.main} onSubmit={handleSubmit}>
+                    <div >
 
 
-                    <div className={style.form__group}>
-                        <div className={style.left}>
-                            <label>First Name</label>
+                        <div className={style.form__group}>
+                            <div className={style.left}>
+                                <label>First Name</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <input type='text'
+                                    required
+                                    name="firstName"
+                                    onChange={handleChange}
+                                />
+                            </div>
+
                         </div>
 
-                        <div className={style.right}>
-                            <input type='text'
-                                required
-                                name="firstName"
-                                onChange={handleChange}
-                            />
+                        <div className={style.form__group}>
+                            <div className={style.left}>
+                                <label>Last Name</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <input type='text'
+                                    name="lastName"
+                                    required
+                                    onChange={handleChange}
+                                />
+                            </div>
+
                         </div>
+
+                        <div className={style.form__group}>
+                            <div className={style.left}>
+                                <label>Email</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <input type='text'
+                                    name="email"
+                                    required
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                        </div>
+
+                        <div className={style.form__group}>
+                            <div className={style.left}>
+                                <label>Gender</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <select name="gender"
+                                    onChange={handleChange}
+                                    required
+                                >
+                                    <option type='text' value="MALE">Male </option>
+                                    <option type='text' value="FEMALE">Female </option>
+
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <div className={style.form__group}>
+                            <div className={style.left}>
+                                <label>Role</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <select name="role"
+                                    required
+                                    onChange={handleChange}
+                                >
+
+                                    <option type='text' value="STAFF">Staff</option>
+                                    <option type='text' value="ADMIN">Admin</option>
+
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <div className={style.form__group}>
+                            <div className={style.left}>
+                                <label>Join Date</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <input type='date'
+                                    required
+                                    name="joinDate"
+                                    onChange={handleChange} />
+                            </div>
+                        </div>
+
+                        <div className={style.form__group}>
+
+                            <div className={style.left}>
+                                <label>Location</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <select name="locationId"
+                                    onChange={handleChange}
+                                    required
+                                    type='number'>
+                                    {locale.map((item) => (
+                                        <option key={item.id} required type='number' value={parseInt(item.id)}>{item.name} </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className={style.form__group}>
+
+                            <div className={style.left}>
+                                <label>Department</label>
+                            </div>
+
+                            <div className={style.right}>
+                                <select name="departmentID"
+                                    onChange={handleChange}
+                                    required
+                                    type='number'>
+                                    {dept.map((item) => (
+                                        <option key={item.id} required type='number' value={parseInt(item.id)}>{item.name} </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+
 
                     </div>
 
                     <div className={style.form__group}>
-                        <div className={style.left}>
-                            <label>Last Name</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <input type='text'
-                                name="lastName"
-                                required
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                    </div>
-
-                    <div className={style.form__group}>
-                        <div className={style.left}>
-                            <label>Email</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <input type='text'
-                                name="email"
-                                required
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                    </div>
-
-                    <div className={style.form__group}>
-                        <div className={style.left}>
-                            <label>Gender</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <select name="gender"
-                                onChange={handleChange}
-                                required
-                            >
-                                <option type='text' value="MALE">Male </option>
-                                <option type='text' value="FEMALE">Female </option>
-
-                            </select>
-                        </div>
-
-                    </div> 
-
-                    <div className={style.form__group}>
-                        <div className={style.left}>
-                            <label>Role</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <select name="role"
-                                required
-                                onChange={handleChange}
-                            >
-                                <option type='text' value="ADMIN">Admin</option>
-                                <option type='text' value="STAFF">Staff</option>
-
-                            </select>
-                        </div>
-
-                    </div>
-
-                    <div className={style.form__group}>
-                        <div className={style.left}>
-                            <label>Join Date</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <input type='date'
-                                required
-                                name="joinDate"
-                                onChange={handleChange} />
+                        <div className={style.btns}>
+                            <button onClick={() => { setIsClosed(!isClosed) }}>Close </button>
+                            <button type="submit">Apply</button>
                         </div>
                     </div>
+                </form>
 
-                    <div className={style.form__group}>
-
-                        <div className={style.left}>
-                            <label>Location</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <select name="locationId"
-                                onChange={handleChange}
-                                required
-                                type='number'>
-                                {locale.map((item) => (
-                                    <option key={item.id} required type='number' value={parseInt(item.id)}>{item.name} </option>
-                                ))}
-                            </select>
-                        </div>
-                    </div>
-
-                     <div className={style.form__group}>
-
-                        <div className={style.left}>
-                            <label>Department</label>
-                        </div>
-
-                        <div className={style.right}>
-                            <select name="departmentID"
-                                onChange={handleChange}
-                                required
-                                type='number'>
-                                {dept.map((item) => (
-                                    <option key={item.id} required type='number' value={parseInt(item.id)}>{item.name} </option>
-                                ))}
-                            </select>
-                        </div>
-                    </div>
-
-
+            }
+            {isClosed &&
+                <div className={style.btns}>
+                    <button onClick={() => { setIsClosed(!isClosed) }}>Create Employee </button>
                 </div>
 
-                <div className={style.form__group}>
-                    <div className={style.btns}>
-                        <button onClick={() => { setIsClosed(!isClosed) }}>Close </button>
-                        <button type="submit">Apply</button>
-                    </div>
-                </div>
-            </form>
+            }
 
-        }
-        {isClosed &&
-            <div className={style.btns}>
-                <button onClick={() => { setIsClosed(!isClosed) }}>Create Employee </button>
-            </div>
-
-        }
-
-    </div>
+        </div>
 
     );
 }
